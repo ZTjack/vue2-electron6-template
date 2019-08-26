25dcb24c01e42e75ffc71168d115187d67650148
ZTjack <jack.jiang.scivan@gmail.com>
2019-08-26 14:31:50 +0800

    upate to latest render structure

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -2,17 +2,15 @@
  * @Author: Jack
  * @Date: 2019-04-04 22:12:42
  * @LastEditors: Jack
- * @LastEditTime: 2019-08-20 17:38:04
- * @Description: 
+ * @LastEditTime: 2019-08-26 12:06:02
+ * @Description:
  */
 import Vue from 'vue'
 import '@/styles/normalize/base.scss' // global css
-import App from './App'
+import App from './layout/Index'
 import router from './router'
 import store from './vuex/store'
-
 Vue.config.productionTip = false
-
 new Vue({
   el: '#app',
   router,
--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: Jack
+ * @Date: 2019-04-04 22:12:42
+ * @LastEditors: Jack
+ * @LastEditTime: 2019-08-26 12:05:12
+ * @Description:
+ */
 import Vue from 'vue'
 import Router from 'vue-router'
 
