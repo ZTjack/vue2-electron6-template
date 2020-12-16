cc0df6448dd5bc042c43e2739a54c29314b838a0
ZTjack <jack.jiang.scivan@gmail.com>
2020-12-16 13:40:23 +0800

    update @ demo

--- src/renderer/views/home/Index.vue
+++ src/renderer/views/home/Index.vue
@@ -2,20 +2,25 @@
  * @Author: Jack
  * @Date: 2019-04-04 22:12:42
  * @LastEditors: Jack
- * @LastEditTime: 2020-06-05 18:09:08
+ * @LastEditTime: 2020-12-16 13:39:53
  * @Description: 
  -->
 <template>
   <div class="home-container">
     home page
+    <page />
   </div>
 </template>
 
 <script>
 import { ipcRenderer } from 'electron'
 import { TEST_CHANNEL } from 'src/main/channel/const'
+import page from '@/views/404.vue'
 export default {
   name: 'Home',
+  components: {
+    page
+  },
   mounted() {
     ipcRenderer.on(TEST_CHANNEL, (event, arg) => {
       console.log('Main sent data', arg)
